db33d1059df7a506099bae7d5793219d3733ea72
Zword <jesseljohn130@gmail.com>
2015-10-15 19:58:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 ! function(window, angular) {
   'use strict';
   angular.module('staticContent', [])
-  .provider('uiDirectiveConfig',[ '$provide', function ($provide) {
+    .provider('staticContent', ['$provide', function($provide) {
 
-    this.dataUrl = null;
+      this.dataUrl = "data.json";
 
-    this.setUrl = function(url) {
-      this.dataUrl = url;
-    },
+      this.setUrl = function(url) {
+        this.dataUrl = url;
+      };
 
-    this.$get = ['$http', function ($http) {
-      var promise = $http({
-        method: "get",
-        url: this.dataUrl
-      });
-      return promise;
+      this.$get = ['$http', '$q', function($http, $q) {
+        var self = this;
+        return {
+          fetchData: function(resource) {
+            var defer = $q.defer();
+            $http({
+              method: "GET",
+              url: resource || self.dataUrl
+            }).then(function(data) {
+              defer.resolve(data);
+            });
 
-    }];
+            return defer.promise;
+          },
+          setUrl:self.setUrl
+        };
+      }];
 
-  }])
-  .directive("ghStatic", function(){
-    return {
-      restrict: 'E',
-      scope: { itemData:  '=' },
-      templateUrl: function(elem, attr){
-        return attr.template + '.html';
-      },
-      replace:true
-    };
-  });
+    }])
+    .directive("ghStatic", function(staticContent) {
+      return {
+        restrict: 'E',
+        scope:true,
+        templateUrl: function(elem, attr) {
+          return attr.template;
+        },
+        controller: function($scope,$element,$attrs) {
+          staticContent.fetchData($attrs.resource).then(function(data) {
+            $scope.staticData = data;
+            console.log(data);
+          });
+        },
+        replace: true
+      };
+    });
 
 }(window, window.angular);

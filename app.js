97f80f74f19376c4f355b93a926dc8b2637dc653
Ankur Mishra <ankish.1106@gmail.com>
2015-10-15 16:07:15 +0530

    removed other files and refactored directive to one

--- app.js
+++ app.js
@@ -1,62 +1,33 @@
-angular.module('uiDirective', [])
-.provider('uiDirectiveConfig',[ '$provide', function ($provide) {
-    
+! function(window, angular) {
+  'use strict';
+  angular.module('staticContent', [])
+  .provider('uiDirectiveConfig',[ '$provide', function ($provide) {
+
     this.dataUrl = null;
-    
+
     this.setUrl = function(url) {
       this.dataUrl = url;
     },
-    
+
     this.$get = ['$http', function ($http) {
       var promise = $http({
-                      method: "get",
-                      url: this.dataUrl
-                    });
-      
+        method: "get",
+        url: this.dataUrl
+      });
       return promise;
 
     }];
 
-}])
-.directive("testimonials", function(){
-  return {
-    restrict: 'E',
-    scope: true,
-    controller: function($scope, uiDirectiveConfig) {
-      $scope.itemData = {
-        testimonials:null,
-        sliderInterval:null
-      };
-      uiDirectiveConfig.success(function(response){
-        $scope.itemData.testimonials = response.testimonials;
-        $scope.itemData.sliderInterval = response.sliderConfig.interval;
-      })
-    },
-    templateUrl: function(elem, attr){
-      return attr.template + '.html';
-    },
-    replace:true
-  };
-})
+  }])
+  .directive("ghStatic", function(){
+    return {
+      restrict: 'E',
+      scope: { itemData:  '=' },
+      templateUrl: function(elem, attr){
+        return attr.template + '.html';
+      },
+      replace:true
+    };
+  });
 
-.directive("pressRelease", function(){
-  return {
-    restrict: 'E',
-    scope: true,
-    controller: function($scope, uiDirectiveConfig) {
-      $scope.itemData = {
-        releases:null
-      };
-      uiDirectiveConfig.success(function(response){
-        $scope.itemData.releases = response.news;
-      })
-    },
-    templateUrl: function(elem, attr){
-      return attr.template+'.html';
-    },
-    replace:true
-  };
-});
-
-
-
+}(window, window.angular);
--- script.js
+++
@@ -1,4 +0,0 @@
-angular.module('myApp', ['uiDirective', 'ui.bootstrap'])
-  .config(function(uiDirectiveConfigProvider){
-  uiDirectiveConfigProvider.setUrl('data.json');
-})

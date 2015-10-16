999a86aedc1dce78b994378cd440ac1ae3fb453e
Amit Dubey <amit@grabhouse.com>
2015-10-16 14:03:03 +0530

    Update app.js
    Added comments.

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
 ! function(window, angular) {
-  'use strict';
-  angular.module('staticContent', [])
-    .provider('staticContent', ['$provide', function($provide) {
+    'use strict';
 
-      this.dataUrl = "data.json";
+    angular.module('staticContent', [])
+        .provider('$staticContent', [function() {
 
-      this.setUrl = function(url) {
-        this.dataUrl = url;
-      };
+            this.dataUrl = "data.json";
 
-      this.$get = ['$http', '$q', function($http, $q) {
-        var self = this;
-        return {
-          fetchData: function(resource) {
-            var defer = $q.defer();
-            $http({
-              method: "GET",
-              url: resource || self.dataUrl
-            }).then(function(data) {
-              defer.resolve(data);
-            });
+            //set the data url.It can be rest api or json file data
+            this.setUrl = function(url) {
+                this.dataUrl = url;
+            };
 
-            return defer.promise;
-          },
-          setUrl:self.setUrl
-        };
-      }];
+            this.$get = ['$http', '$q', function($http, $q) {
+                var self = this;
+                return {
+                    fetchData: function(resource) {
+                        var defer = $q.defer();
+                        $http({
+                            method: "GET",
+                            url: resource || self.dataUrl
+                        }).then(function(data) {
+                            defer.resolve(data);
+                        });
 
-    }])
-    .directive("ghStatic", function(staticContent) {
-      return {
-        restrict: 'E',
-        scope:true,
-        templateUrl: function(elem, attr) {
-          return attr.template;
-        },
-        controller: function($scope,$element,$attrs) {
-          staticContent.fetchData($attrs.resource).then(function(data) {
-            $scope.staticData = data;
-          });
-        },
-        replace: true
-      };
-    });
+                        return defer.promise;
+                    },
+                    setUrl: self.setUrl
+                };
+            }];
+
+        }])
+        .directive("ghStatic", function($staticContent) {
+            return {
+                restrict: 'E',
+                scope: true,
+                templateUrl: function(elem, attr) {
+                    return attr.template;
+                },
+                controller: function($scope, $element, $attrs) {
+                    $staticContent.fetchData($attrs.resource).then(function(data) {
+                        $scope.staticData = data;
+                    });
+                },
+                replace: true
+            };
+        });
 
 }(window, window.angular);
